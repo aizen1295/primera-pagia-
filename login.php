b2317f34f192930758b05782698571c6e344b3ab
aizen1295 <david_forerope@fet.edu.co>
2019-08-24 10:08:17 -0500

    cambiar index por login

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>inicio de seccion</title>
+</head>
+
+<body>
+
+    <div class="login-uno">
+        <img class="avatar" src="img/logo.jpeg" alt="logo de la fet">
+        <h1>inicio sección</h1>
+
+        <form action="">
+            <!-- USERNAME -->
+            <div class="form-group">
+                <label for=username>usuario</label>
+                <input id=username class="form-control" type="text" name="" placeholder="ingresar usuario">
+            </div>
+
+            <!-- PASSWORD -->
+            <div class="form-group">
+                <label for=password>constraseña</label>
+                <input id=password class="form-control" type="password" name="" placeholder="ingrasar contraseña">
+            </div>
+
+            <!-- BOTONES Y ENLACES -->
+            <button class="btn btn-success btn-lg btn-block" type="submit">inicio</button>
+            <a href="#"><h6>Olvido su contraseña ?</h6></a>
+            <a href="#"><h6>Registrarse</h6></a>
+        </form>
+    </div>
+    <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
